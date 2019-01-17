7674c7dc55481fd55ac85ea0827a92dd7b88081d
Hwarang Kim <checheche12@unist.ac.kr>
2019-01-18 06:57:12 +0900

    First day...

---
+++ Assets/Script/MultiRace/MultiRaceCharacter/CharacterMoveScripts.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CharacterMoveScripts : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasAuthority)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(new Vector3(0, -Time.deltaTime * 40.0f, 0));
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(new Vector3(0, Time.deltaTime * 40.0f, 0));
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += (transform.forward) * 0.5f;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position -= (transform.forward) * 0.5f;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += (transform.up) * 0.5f;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position -= (transform.up) * 0.5f;
+            }
+        }
+    }
+}

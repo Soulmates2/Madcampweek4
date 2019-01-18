eed981a2732258c027e11a7aa77833a96d080329
kimkunho <kaist984@kaist.ac.kr>
2019-01-18 21:02:02 +0900

    scare

--- Assets/Script/MultiRace/MultiRaceCharacter/CharacterMoveScripts.cs
+++ Assets/Script/MultiRace/MultiRaceCharacter/CharacterMoveScripts.cs
@@ -6,6 +6,9 @@ using UnityEngine.Networking;
 public class CharacterMoveScripts : NetworkBehaviour
 {
     public Camera PlayerCamera;
+    public Vector3 speed;
+    float aaaa;
+    public Rigidbody PlayerRigidBody;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,18 +33,49 @@ public class CharacterMoveScripts : NetworkBehaviour
             {
                 transform.Rotate(new Vector3(0, -Time.deltaTime * 80.0f, 0));
             }
+
+
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 transform.Rotate(new Vector3(0, Time.deltaTime * 80.0f, 0));
             }
+
+
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                transform.position += (transform.forward) * 0.25f;
+                Debug.Log("aaaa");
+                if (aaaa <= 0.5f)
+                {
+                    Debug.Log("aaaa");
+                    aaaa += 0.003f;
+                    transform.position += (transform.forward) * aaaa;
+                    //speed += new Vector3(0, 0, 0.1f);
+                    //PlayerRigidBody.velocity = speed;
+                    //PlayerRigidBody.AddForce(0, 0, 80f * Time.deltaTime);
+                    //transform.GetComponent<Rigidbody>().AddForce(0,0, 80f * Time.deltaTime);
+                    Debug.Log("aaaa");
+                }
+                else
+                {
+                    transform.position += (transform.forward) * aaaa;
+                }
             }
+            else
+            {
+                if (aaaa > 0)
+                {
+                    aaaa -= 0.003f;
+                    transform.position += (transform.forward) * aaaa;
+                }
+            }
+
+
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 transform.position -= (transform.forward) * 0.25f;
             }
+
+
             if (Input.GetKey(KeyCode.W))
             {
                 transform.position += (transform.up) * 0.5f;

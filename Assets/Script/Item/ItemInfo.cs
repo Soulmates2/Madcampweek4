f4b1b25480c1ce7f4aaa2973badf27079fa076db
kimkunho <kaist984@kaist.ac.kr>
2019-01-19 17:26:42 +0900

    Add Item Script

---
+++ Assets/Script/Item/ItemInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ItemInfo : NetworkBehaviour
+{
+    public TypeScript type;
+    public GameObject item;
+    string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            type = item.GetComponent<TypeScript>();
+            type.SetTypeScript("item");
+        }
+        catch
+        {
+
+        }
+    }
+    private void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SetName(string name)
+    {
+        this.name = name;
+    }
+}
---
+++ Assets/Script/Item/ItemList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemList
+{
+    private static string [] list_of_item = { "none", "Image/MulitiGamePlay/MultiGamePlay" };
+    // Start is called before the first frame update    
+    public static string GetItemName(int i)
+    {
+        return list_of_item[i];
+    }
+}

81512cc14ccef6688b043247fe97ad499b3ba101
Younggyun Brian Yoon <yyg470603@gmail.com>
2019-01-19 23:08:59 +0900

    Speedometer

---
+++ Assets/testing/moveneddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveneddle : MonoBehaviour
+{
+
+    public GameObject neddle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            neddle.transform.Rotate(0.0f, 0.0f, -0.5f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            neddle.transform.Rotate(0.0f, 0.0f, 0.5f);
+        }
+    }
+}

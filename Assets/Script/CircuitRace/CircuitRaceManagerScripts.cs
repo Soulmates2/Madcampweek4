d9044159deb2a9f1025f7235ca1ed67a966dc057
Hwarang Kim <checheche12@unist.ac.kr>
2019-01-19 21:09:04 +0900

    Add

--- Assets/Script/CircuitRace/CircuitRaceManagerScripts.cs
+++ Assets/Script/CircuitRace/CircuitRaceManagerScripts.cs
@@ -18,13 +18,14 @@ public class CircuitRaceManagerScripts : MonoBehaviour
         Instantiate(Map[CircuitRaceManager.MapKind]);
 
         GameObject[] StartPositionObject = GameObject.FindGameObjectsWithTag("StartPosition");
-        Player = Instantiate(Car[CircuitRaceManager.CarKind], StartPositionObject[0].transform.position, Quaternion.Euler(0, 0, 0) );
+        Player = Instantiate(Car[CircuitRaceManager.CarKind], StartPositionObject[0].transform.position, Quaternion.Euler(0, 0, 0));
+        
         Player.GetComponentInChildren<CharacterMoveScripts>().Is_MyCharacter = true;
 
         AI = new GameObject[7];
         for(int i = 0; i < 7; i++)
         {
-            AI[i] = Instantiate(Car[Random.Range(0, Car.Length)], StartPositionObject[i + 1].transform.position, Quaternion.Euler(0, 0, 0));
+            AI[i] = Instantiate(Car[Random.Range(0, Car.Length)], StartPositionObject[i].transform.position, Quaternion.Euler(0, 0, 0));
             AI[i].GetComponentInChildren<Camera>().enabled = false;
             AI[i].GetComponentInChildren<CharacterMoveScripts>().Is_MyCharacter = false;
         }

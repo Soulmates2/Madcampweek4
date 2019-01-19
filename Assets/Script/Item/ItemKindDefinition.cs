5e3532e1bca2f3f47f48df883c7f6309b86e5273
Hwarang Kim <checheche12@unist.ac.kr>
2019-01-20 02:37:17 +0900

    20190120 0237
    Former-commit-id: a450f9863411d8ed6d8cd90505d4c06629d3b250

---
+++ Assets/Script/Item/ItemKindDefinition.cs
@@ -0,0 +1,24 @@
+﻿public static class ItemKindDefinition
+{
+    public static int NO_ITEM = 0;
+
+    public static int BLUE_FIRST = 101;
+    public static int BLUE_SECOND = 102;
+    public static int BLUE_THIRD = 103;
+    public static int BLUE_ULTIMATE = 104;
+
+    public static int RED_FIRST = 201;
+    public static int RED_SECOND = 202;
+    public static int RED_THIRD = 203;
+    public static int RED_ULTIMATE = 204;
+
+    public static int GREEN_FIRST = 301;
+    public static int GREEN_SECOND = 302;
+    public static int GREEN_THIRD = 303;
+    public static int GREEN_ULTIMATE = 304;
+
+    public static int YELLOW_FIRST = 401;
+    public static int YELLOW_SECOND = 402;
+    public static int YELLOW_THIRD = 403;
+    public static int YELLOW_ULTIMATE = 404;
+}

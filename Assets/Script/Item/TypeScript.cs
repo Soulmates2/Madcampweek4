07b0c0b22071acb53af6e8fafc3986509e5cd79b
kimkunho <kaist984@kaist.ac.kr>
2019-01-19 17:26:42 +0900

    Add Item Script

---
+++ Assets/Script/Item/TypeScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeScript : MonoBehaviour
+{
+    public string type;
+    public bool isPlayer = false;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetTypeScript(string str)
+    {
+        type = str;
+        if (str.Equals("others") || str.Equals("player"))
+        {
+            isPlayer = true;
+        }
+
+    }
+    public string GetTypeScript()
+    {
+        return type;
+    }
+    public bool IsPlayer()
+    {
+        return isPlayer;
+    }
+}

04484720d4a90619a4080377062ce5ca0d599ee1
Hwarang Kim <checheche12@unist.ac.kr>
2019-01-21 21:41:05 +0900

    Item

---
+++ Assets/Script/Item/ItemRespone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRespone : MonoBehaviour
+{
+    private bool IsITItem = false;
+    public GameObject[] Item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ItemMake();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsITItem)
+        {
+            IsITItem = false;
+            Invoke("ItemMake", 10.0f);
+        }
+    }
+
+    public void ItemMake()
+    {
+        if (!IsITItem)
+        {
+            IsITItem = true;
+            var i = Instantiate(Item[Random.Range(0, Item.Length)], transform.position, Quaternion.Euler(0, 0, 45));
+            i.transform.parent = transform;
+        }
+    }
+
+}
